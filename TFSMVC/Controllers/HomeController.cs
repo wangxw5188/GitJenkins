a2fcca07badb87eb2cc1e66a5d18bda8fce4b570
wangxiaowei <wangxiaowei@wochu.cn>
2017-06-23 15:36:28 +0800

    1

---
+++ TFSMVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TFSLib;
+
+namespace TFSMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Demo demo = new Demo();
+            demo.Sub();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TFSMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TFSMVC.MvcApplication" Language="C#" %>
